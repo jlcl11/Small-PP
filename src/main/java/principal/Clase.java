0b6d41bdee5b32da50cdab2bafe9fd198a245cea
José Luis Corral López <jcorrallopez8@gmail.com>
2021-10-20 19:25:03 +0200

    Otro cambio
    En esta línea se empieza a desarrollar la rama de Gonzalo

--- src/main/java/principal/Clase.java
+++ src/main/java/principal/Clase.java
@@ -4,5 +4,6 @@ public class Clase {
 		System.out.println("Érase una vez en CENEC");
 		System.out.println("Aprendíamos a usar GitHub");
 		System.out.println("Sin embargo,el empezar con los repositorios descubrimos el coñazo que es empezar con el programa");
+		System.out.println("Entonces después de hora y media ,Gonzalo saltó de los nervios y maldijo al Miguel en repetidas ocasiones");
 	}
 }
