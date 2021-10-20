5a5dae4ed3c52b5985ce36cb116c7332260da79a
José Luis Corral López <jcorrallopez8@gmail.com>
2021-10-20 19:40:01 +0200

    Línea 10
    Gonzalo body reveal

--- src/main/java/principal/Clase.java
+++ src/main/java/principal/Clase.java
@@ -7,5 +7,10 @@ public class Clase {
 		System.out.println("Entonces después de hora y media ,Gonzalo saltó de los nervios y maldijo al Miguel en repetidas ocasiones");
 		System.out.println("Debido a la ofensa,Miguel se decidió a suspender a Gonzalo por todo lo que queda de curso y el curso que viene");
 		System.out.println("Como Gonzalo no quería tener una asignatura suspensa,sacó toda la furia que adquirío en el ejército y el resto de alumnos pudieron notar como el aire estaba enrarecido");
+	System.out.println("Por sorpresa,Gonzalo desarrolló durante su tiempo sirviendo a la patria un estado físico superior");
+	
+	
+	
+	
 	}
 }
