60020f509120edf46d1c846bb250e71cc963bbf2
José Luis Corral López <jcorrallopez8@gmail.com>
2021-10-20 18:46:09 +0200

    Segunda versión
    Añado otra línea de código

--- src/main/java/principal/Clase.java
+++ src/main/java/principal/Clase.java
@@ -2,7 +2,7 @@ package principal;
 public class Clase {
 	public static void main(String[] args) {
 		System.out.println("Érase una vez en CENEC");
-
+		System.out.println("Aprendíamos a usar GitHub");
 	}
 
 }
